5fe9d367925dd1e060547ba4013c61102124a222
raymondnes <raynelreign@gmail.com>
2022-12-08 10:56:43 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 /**
-*main - Entry point
-*Retun - Always 0 (success)
+ *main - Entry point
+ *Retun - Always 0 (success)
 */
 
 int main(void)
-
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
