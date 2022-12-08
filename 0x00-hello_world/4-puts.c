ba369bbc93baecfa4e23f70d4321c077aa68a65c
raymondnes <raynelreign@gmail.com>
2022-12-08 02:58:37 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
 #!/bin/bash
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
 
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
-
-
 
 			return (0);
 
