14b8082e30f3069bbf40667e43ac56179a0c3c3e
raymondnes <raynelreign@gmail.com>
2022-12-09 14:51:19 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabets
+ *
+ * Return - Always 0 (Success)
+ */
+
+int main(void)
+{
+   
+          int ch = 'a';
+
+	  while (ch <= 'z')
+          {  
+	
+		  putchar (ch);
+		  ch++;   
+	  
+	  }
+	  putchar('\n');
+	  return (0); 
+
+}
