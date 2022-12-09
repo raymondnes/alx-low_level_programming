db1dcb49975799573f76d51eca07437888b3b1e6
raymondnes <raynelreign@gmail.com>
2022-12-09 14:20:10 -0800

    last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,65 +1,34 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
-
-
 /**
+ * main -main function to generate a random number
  *
- *  * main -main function to generate a random number
+ * Return: Always 0 (Success)
  *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     *
- *
- *      */
-
-
+ */
 
 int main(void)
 
 {
 
-		int n;
+	int n;
 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-
-
-					if (n > 0)
-
-							{
-
-										printf("%d is positive\n", n);
-
-											}
-
-						else if (n == 0)
-
-								{
-
-											printf("%d is zero\n", n);
-
-												}
-
-							else if (n < 0)
-
-									{
-
-												printf("%d is negative\n", n);
-
-													}
-
-								return (0);
-
-
-
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
 }
