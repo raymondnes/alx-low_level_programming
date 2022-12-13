cd7adad4f5cc28bf68bbc99111bd99b0f0d4f400
raymondnes <raynelreign@gmail.com>
2022-12-13 04:50:43 -0800

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -32,19 +32,19 @@ int numLength(int num)
 int main(void)
 {
 	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, f20 = 0, tmpo = 0;
-	short int i = 1 initial0s;
+	short int i = 1, initial0s;
 
 	while (i <= 98)
 	{
 		if (f1o > 0)
-			printf("%1u", f1o);
+			printf("%lu", f1o);
 		initial0s = numLength(mx) - 1 - numLength(f1);
 		while (f1o > 0 && initial0s > 0)
 		{
 			printf("%i", 0);
 			initial0s--;
 		}
-		printf("%1u", f1);
+		printf("%lu", f1);
 
 		tmp = (f1 + f2) % mx;
 		tmpo = f1o + f2o + (f1 + f2) / mx;
