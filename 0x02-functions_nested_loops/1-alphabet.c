088faea5c4c5bb6d4fa919b7c7c2afa9f506cc38
raymondnes <raynelreign@gmail.com>
2022-12-13 00:32:45 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "printalphabet.h"
+
+/**
+ * print_alphabets - prints lowercase alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
