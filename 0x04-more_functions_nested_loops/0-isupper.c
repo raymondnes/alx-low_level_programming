f2bdc5ae99d8c0ae9214c048ae43b2144249ac16
raymondnes <raynelreign@gmail.com>
2022-12-15 03:06:04 -0800

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,9 +8,14 @@
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	return (1);
-
-	else
-	return (0);
+char uppercase = 'A';
+int isupper = '0';
+for (; uppercase <= 'Z'; uppercase++)
+{
+	if (c == uppercase)
+	{
+		isupper = 1;
+		break;
+	}
+	return (isupper);
 }
