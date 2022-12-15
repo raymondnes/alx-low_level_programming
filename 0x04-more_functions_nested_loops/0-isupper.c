90f5bffb2ab7cbdb57272fe3a1a7be4305925384
raymondnes <raynelreign@gmail.com>
2022-12-15 01:24:45 -0800

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _isupper - A function that checks for uppercase character
+  * @c: An input character
+  * Return: 1 if c is uppercase or 0 otherwise
+  */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
