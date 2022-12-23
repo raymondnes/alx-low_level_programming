7e1b5d0bfe5a1ed2ea260497f2749ae906b33469
raymondnes <raynelreign@gmail.com>
2022-12-22 22:43:03 -0800

    Noise is a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,49 +3,59 @@
 
 
 /**
-  * print_buffer - prints buffer
-  * @b: buffer
-  * @size: size
+  * print_line - prints a s bytes of a buffer
+  * @c: buffer to print
+  * @s: bytes of buffer to print
+  * @l: line of buffer to print
   * Return: void
   */
 
+void print_line(char *c, int s, int l)
+{
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+	}
+}
+
+/**
+  * print_buffer - prints a buffer
+  * @b: buffer to print
+  * @size: size of buffer
+  *
+  * Return: void
+  */
 void print_buffer(char *b, int size)
 {
-	int o, j, i;
+	int i;
 
-	o = 0;
-
-	if (size <= 0)
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
-		printf("\n");
-		return;
-	}
-	while (o < size)
-	{
-		j = size - o < 10 ? size - o : 10;
-		printf("%08x: ", o);
-		for (i = 0; i < 10; i++)
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
 		{
-			if (i < j)
-				printf("%02x", *(b + o + i));
-			else
-				printf("  ");
-			if (i % 2)
-			{
-				printf(" ");
-			}
-
-		for (i = 0; i < j; i++)
+			print_line(b, 9, i);
 		{
-			int c = *(b + o + i);
-
-			if (c < 32 || c > 132)
-			{
-				c = '.';
-			}
-			printf("%c", c);
+		else
+		{
+			print_line(b, size % 10 - 1, i);
 		}
-		printf("\n");
-		o += 10;
+		putchar('\n');
 	}
+	if (size == 0)
+		putchar('\n');
 }
