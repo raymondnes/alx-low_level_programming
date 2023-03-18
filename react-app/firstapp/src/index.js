97409ba62642379957c067fec5615184f0ba8ed1
raymondnes <raynelreign@gmali.com>
2023-03-18 15:47:01 +0400

    update

---
+++ react-app/firstapp/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello World, here's my first react application</h1>;
+ReactDOM.render(element, document.getElementById("root"));
